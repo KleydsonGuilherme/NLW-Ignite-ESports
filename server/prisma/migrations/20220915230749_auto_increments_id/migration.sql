770e10fe3e04de25af4167df9e572bcdf92ccc37
Kleydson Guilherme <kleydsong@gmail..com>
2022-09-16 11:38:51 -0300

    Primeiro Commit do NLW E-Sports

---
+++ server/prisma/migrations/20220915230749_auto_increments_id/migration.sql
@@ -0,0 +1,28 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Ad" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "gameId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "weekDays" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "hourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Ad" ("createdAt", "discord", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearPlaying") SELECT "createdAt", "discord", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearPlaying" FROM "Ad";
+DROP TABLE "Ad";
+ALTER TABLE "new_Ad" RENAME TO "Ad";
+CREATE TABLE "new_Game" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "bannerUrl" TEXT NOT NULL
+);
+INSERT INTO "new_Game" ("bannerUrl", "id", "title") SELECT "bannerUrl", "id", "title" FROM "Game";
+DROP TABLE "Game";
+ALTER TABLE "new_Game" RENAME TO "Game";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
